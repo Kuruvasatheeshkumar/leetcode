9bef640a6bb801d2f28bc2807178f224f4835556
Kuruvasatheeshkumar <satheeshkumar68019@gmail.com>
2026-08-11 19:02:16 +0530

    Time: 0 ms (100%), Space: 42.9 MB (68.69%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String interpret(String command) {
+        StringBuilder result = new StringBuilder();
+        int n = command.length();
+        int i = 0;
+
+        while (i < n) {
+            if (command.charAt(i) == 'G') {
+                result.append('G');
+                i++;
+            } else if (command.startsWith("()", i)) {
+                result.append('o');
+                i += 2;
+            } else if (command.startsWith("(al)", i)) {
+                result.append("al");
+                i += 4;
+            }
+        }
+
+        return result.toString();
+    }
+}
